18cc82f3ab3a457f2f343bfa2904a7e8d3c11bf7
hugo <cleryhugo62@gmail.com>
2025-03-28 13:16:07 +0100

    fix: remove unused code

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slices/user.slice";
 import authReducer from "./slices/auth.slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    user: userReducer,
   },
   devTools: true,
 });
